0727ede9345b47dc7e0ec3827bb673795f357674
Cain Atkinson <yellowsink@riseup.net>
2022-05-21 16:38:21 +0100

    fix build step lmao

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 rm -rf dist
 
-npm run build:snippet
-mv dist/assets/*.js dist/snippet.js
-rmdir dist/assets
-rm dist/index.html
-
 npm exec tsc
+
+npm run build:snippet
+mv dist/assets/*.js tsdist/snippet.js
+rm tsdist/snippet.d.ts
+rm -rf dist
+
+mv tsdist dist
