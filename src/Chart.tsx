0814c0b0c5cb3ba9326c131e3fb9f70e805fdfe3
Cain Atkinson <19270622+yellowsink@users.noreply.github.com>
2022-05-28 11:36:36 +0000

    idrk uh yeah

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,13 +1,18 @@
-import { Component, createEffect } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 
 const height = 100;
 const width = 200;
 
-const max = (vals: number[]) => vals.reduce((p, c) => Math.max(p ?? 0, c ?? 0));
-const min = (vals: number[]) => vals.reduce((p, c) => Math.min(p ?? 0, c ?? 0));
+const calcMax = (vals: number[]) =>
+  vals.reduce((p, c) => Math.max(p ?? 0, c ?? 0));
+const calcMin = (vals: number[]) =>
+  vals.reduce((p, c) => Math.min(p ?? 0, c ?? 0));
 
 const Chart: Component<{ data: number[] }> = (props) => {
   let canvas: HTMLCanvasElement;
+
+  const [min, setMin] = createSignal(0);
+  const [max, setMax] = createSignal(0);
 
   createEffect(() => {
     const data = props.data;
@@ -16,9 +21,11 @@ const Chart: Component<{ data: number[] }> = (props) => {
     ctx.clearRect(0, 0, 200, 100);
     ctx.beginPath();
 
+    const dataMin = calcMin(data);
+    const dataMax = calcMax(data);
     for (let i = 0; i < data.length; i++) {
-      const ceil = min(data);
-      const floor = max(data) - ceil;
+      const ceil = dataMin;
+      const floor = dataMax - dataMin;
 
       const multiplier = (data[i] - ceil) / floor;
       const pointHeight = (1 - multiplier) * height;
@@ -29,12 +36,23 @@ const Chart: Component<{ data: number[] }> = (props) => {
     ctx.strokeStyle = "white";
     ctx.stroke();
 
-    ctx.moveTo(0, 0);
-    ctx.strokeText(max(data).toFixed(2) + "ms", 0, 10);
-    ctx.strokeText(min(data).toFixed(2) + "ms", 0, height);
+    setMin(dataMin);
+    setMax(dataMax);
   });
 
-  return <canvas ref={canvas} width={width} height={height} />;
+  // noinspection JSUnusedAssignment - the ref={canvas}
+  return (
+    <div style="display:grid;justify-items:right">
+      {max().toFixed(2)}ms
+      <span style="align-self:end">{min().toFixed(2)}ms</span>
+      <canvas
+        style="grid-area:1/2/3/3"
+        ref={canvas}
+        width={width}
+        height={height}
+      />
+    </div>
+  );
 };
 
 export default Chart;
--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,50 +1,53 @@
-import type { Component } from "solid-js";
-import { Show } from "solid-js";
+import type {Component} from "solid-js";
+import {Show} from "solid-js";
 import usePerf from "./usePerf";
 import Chart from "./Chart";
 
 const Overlay: Component<{ c: boolean; setC: (b: boolean) => void }> = (
-  props
+	props
 ) => {
-  const perf = usePerf(() => props.c);
+	const perf = usePerf(() => props.c);
 
-  return (
-    <Show when={!props.c}>
-      <div
-        style={{
-          background: "#000c",
-          margin: ".5rem",
-          padding: ".5rem",
-          width: "max-content",
-          color: "#fff",
-          "font-family": "monospace",
-        }}
-      >
-        <Show when={perf.sus()}>
-          Frame times are suspicious
-          <br />
-          If on Firefox, ensure privacy.resistFingerprinting is off
-          <br />
-          <br />
-        </Show>
-        FTime: {perf.time()?.toPrecision(4)}ms
-        <br />
-        Current: {perf.fps()?.toPrecision(4)}fps
-        <br />
-        Last 50 mean: {perf.mean()?.toPrecision(4)}fps
-        <br />
-        <button
-          style="background:none;border:1px solid white;color:white"
-          onclick={() => props.setC(true)}
-        >
-          Stop monitoring
-        </button>
-        <br />
-        Frame time graph:
-        <br />
-        <Chart data={perf.raw()} />
-      </div>
-    </Show>
-  );
+	return (
+		<Show when={!props.c}>
+			<div
+				style={{
+					background: "#000c",
+					margin: ".5rem",
+					padding: ".5rem",
+					width: "max-content",
+					color: "#fff",
+					"font-family": "monospace",
+					display: "grid",
+					"grid-template-columns": "auto 1fr",
+					gap: ".25rem"
+				}}
+			>
+				<Show when={perf.sus()}>
+					<div style="grid-column:1/3">
+						Frame times are suspicious
+						<br/>
+						If on Firefox, ensure privacy.resistFingerprinting is off
+					</div>
+				</Show>
+				<span>FTime:</span>
+				<span>{perf.time()?.toPrecision(4)}ms</span>
+				<span>Current:</span>
+				<span>{perf.fps()?.toPrecision(4)}fps</span>
+				<span>Last 50 mean:</span>
+				<span>{perf.mean()?.toPrecision(4)}fps</span>
+				<button
+					style="background:none;border:1px solid white;color:white;grid-column:1/3"
+					onclick={() => props.setC(true)}
+				>
+					Stop monitoring
+				</button>
+				<div style="grid-column:1/3">
+					Frame time graph:
+					<Chart data={perf.raw()}/>
+				</div>
+			</div>
+		</Show>
+	);
 };
 export default Overlay;
